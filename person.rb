9ca9fbbe1f315ac85c4261974ad4915916c2850a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-04 21:01:09 +0500

    modify it to inherit from main class

--- person.rb
+++ person.rb
@@ -1,8 +1,12 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(name = 'unknown', age = nil, parent_permission: true)
+  def initialize(age = nil, name = 'unknown', parent_permission: true)
     @name = name
     @id = rand(1..1000)
     @age = age
@@ -13,9 +17,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age && @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = Capitalize_Decorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = Trimmer_Decorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
