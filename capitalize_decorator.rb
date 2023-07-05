9ea2d2d3f041da4d444c1d5af19b1d0e2e4ed507
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-06 02:49:42 +0500

    modify

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    capitalized_words = @nameable.correct_name.chars.map(&:capitalize)
+    capitalized_words = @object.correct_name.chars.map(&:capitalize)
     capitalized_words.join
   end
 end
