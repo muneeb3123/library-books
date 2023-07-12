1c716def0dc07d8fb7fb6a698bbb2c1422b7aa5b
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 04:10:46 +0500

    add unit test to student

---
+++ spec/student_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../student'
+
+describe Student do
+    let(:classroom) { Classroom.new('Math') }
+    let(:student) {Student.new}
+
+    it 'should initialize all attributes' do
+        student = Student.new('muneeb', 21, parent_permission: false)
+        expect(student).to have_attributes(name: 'muneeb', age: 21, parent_permission: false )
+    end
+
+    it 'should be child of parent and initialize it' do
+        student = Student.new('muneeb', 21, parent_permission: false)
+        expect(student).to be_a(Person)
+    end
+
+    it 'assigns the classroom' do
+        student.add_classroom(classroom)
+        expect(student.classroom).to eq(classroom)
+    end
+
+    it 'adds the student to the classroom' do
+        student.add_classroom(classroom)
+        expect(classroom.students).to include(student)
+    end
+
+    it 'does not add the student to the classroom if already present' do
+        classroom.add_student(student)
+        expect { student.add_classroom(classroom) }.not_to(change { classroom.students.count })
+      end
+end
