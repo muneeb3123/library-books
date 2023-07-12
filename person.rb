46b8e940c12bd36bb41aa22a3fe211f220852f80
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 03:37:48 +0500

    create branch for unit test

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
   attr_reader :id, :rentals
 
   def initialize(age = nil, name = 'unknown', id: rand(1..1000), parent_permission: true)
@@ -34,10 +34,3 @@ class Person < Nameable
     @age && @age >= 18
   end
 end
-
-# person = Person.new(22, 'maximilianus')
-# puts person.correct_name
-# capitalized_person = CapitalizeDecorator.new(person)
-# puts capitalized_person.correct_name
-# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-# puts capitalized_trimmed_person.correct_name
