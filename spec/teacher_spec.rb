72f78c3dbd7c91e9ecb6f10f92d8e1736538bf85
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 04:57:07 +0500

    write test for teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../teacher'
+
+describe Teacher do
+    let(:teacher) { Teacher.new('Computer Science', 'Javed Shbaz', 31) }
+    it 'should initialize all attributes' do
+        expect(teacher.name).to eql('Javed Shbaz')
+        expect(teacher.specialization).to eql('Computer Science')
+        expect(teacher.age).to eql(31)
+    end
+
+    it 'should inherit from the Person class' do
+        expect(teacher).to be_a(Person)
+    end
+
+    it 'should be true' do
+    expect(teacher.can_use_services?).to eql(true)
+    end
+end
